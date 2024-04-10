5dabff0f6a935ba2d858d109b04a989a29a6f4a5
sky <mhn020612@gmail.com>
2024-04-10 20:06:19 +0900

    design: 비밀번호찾기 페이지 퍼블리싱

--- campusconnect/src/pages/FindPasswordPage.jsx
+++ campusconnect/src/pages/FindPasswordPage.jsx
@@ -1,12 +1,25 @@
 import NavBar from '../components/NavBar';
+import styled from 'styled-components';
+import FindPassWordTitle from '../components/findPassword/FindPassWordTitle.jsx';
+import FindPasswordForm from '../components/findPassword/FindPasswordForm.jsx';
+import Footer from '../components/Footer.jsx';
 
 function FindPasswordPage() {
   return (
     <>
       <NavBar />
-      <div>비밀번호 찾기 페이지입니다.</div>
+      <StyledWrapper>
+        <FindPassWordTitle />
+        <FindPasswordForm/>
+      </StyledWrapper>
+      <Footer/>
     </>
   );
 }
 
 export default FindPasswordPage;
+
+const StyledWrapper = styled.div`
+  margin-top: 100px;
+  height: 100%;
+`;
